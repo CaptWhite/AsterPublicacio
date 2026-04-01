2de6e8d67c4dcc9fff42f49c5d51f3e9906f298e
Captwhite <captwhite57@gmail.com>
2026-04-01 13:20:18 +0200

    change images header

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -12,7 +12,7 @@ export const Head = ({ id, date }: Props) => {
         <tr>
           <td>
             <table 
-              background="https://res.cloudinary.com/captwhite-angularadv/image/upload/v1700850212/Aster/Sol.png">
+              background="https://lh3.googleusercontent.com/pw/AP1GczPuWxKc-M1M7NGWS1ajkVcYr0FBu3ozgfMDFbreuLiObTCXNmT7MgQNixAK8anFOgTT42d1Dp0uA7gaHgvqwRlggZJ3515CoVDDv8Iyp6UzHtjhzgYyMDWtDPhG-4f9pQkTYws6PyKtWKbFAf8Zrw-W=w1369-h339-s-no-gm">
               <tbody>
                 <tr>
                   <td>
@@ -21,7 +21,7 @@ export const Head = ({ id, date }: Props) => {
                         <tr>
                           <td style={{ padding: '15px 15px 5px' }}>
                             <div>
-                              <img src="https://www.aster.cat/images/newsletter.jpg"
+                              <img src="https://lh3.googleusercontent.com/pw/AP1GczOc1x0mb3jnVc61xBi4qaxDCFRgsSQnPB710YtupsFTMB_d17feykT08k4S6z9rkL2_9DSdFIN4T8Zw5ObhdtcBBBzqzhulBswESgxNIe0qprSOmRaYkwo0s1FaciTXvgmM6JQ2C23RmCzEaZ26Zkwe=w1115-h362-s-no-gm?authuser=2"
                                 alt="imagen de aster" width={768} height={249} />
                             </div>
                           </td>
